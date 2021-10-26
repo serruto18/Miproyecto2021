471f35028edac1187792ba70c141f8d0d22bf959
Erik Serruto <erik_887@hotmail.com>
2021-10-25 23:50:04 -0400

    Agregando sweetAlert1.js para alertas

--- sweetAlert1.js
+++ sweetAlert1.js
@@ -1,4 +1,28 @@
-function eliminar (){
+/*var confirmed = false;
+$("#boton1").on("submit", function(e) {
+    var $this = $(this);
+    if (!confirmed) {
+        e.preventDefault();
+        swal({
+            title: $(this).data("swa-title"),
+            text: $(this).data("swa-text"),
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#cc3f44",
+            confirmButtonText: $(this).data("swa-btn-txt"),
+            closeOnConfirm: true,
+            html: false
+        }, function() {
+            confirmed = true;
+            $this.submit();
+        });
+    }
+});*/
+
+
+
+function eliminar (indice){
+  
 	Swal.fire({
   title: 'Estas seguro de Eliminar?',
   icon: 'warning',
@@ -8,12 +32,29 @@ function eliminar (){
   confirmButtonText: 'Si, Eliminar!'
 }).then((result) => {
   if (result.isConfirmed) {
+    var int=indice-1;
+   document.forms[int].submit();
     Swal.fire(
-      'Deleted!',
+      'Eliminado!',
       'Your file has been deleted.',
       'success'
     )
+    
   }
 })
 }
- 
+
+function positvoEmpelado() {
+  Swal.fire(
+  'Buen trabajo!',
+  'Se registro el empleado!',
+  'success'
+)
+}
+function positvoInspector() {
+  Swal.fire(
+  'Buen trabajo!',
+  'Se registro al Inspector!',
+  'success'
+)
+}
